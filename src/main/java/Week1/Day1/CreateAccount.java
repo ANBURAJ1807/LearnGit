7aae3fa84808022334b0d781f5de0ece42761f2e
vidya <vidyar1926@gmail.com>
2025-06-02 13:15:40 +0530

    Removed assertion

--- src/main/java/Week1/Day1/CreateAccount.java
+++ src/main/java/Week1/Day1/CreateAccount.java
@@ -32,7 +32,7 @@ public class CreateAccount {
         driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
         String toastMessage = driver.findElement(By.xpath("//span[@class='toastMessage slds-text-heading--small forceActionsText']")).getText();
         System.out.println("Verified the account creation : "+toastMessage);
-        Assert.assertTrue(toastMessage.contains(accountName),"Verify the Account name");
+        
     }
 
 }
